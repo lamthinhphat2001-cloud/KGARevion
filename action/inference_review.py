731e22e9011a562f627c4c69e62c027d7517a382
Sankar <lamthinhphat2001@gmail.com>
2026-06-21 19:31:25 +0700

    Update offload

--- action/inference_review.py
+++ action/inference_review.py
@@ -73,7 +73,8 @@ class ReviewInfer(object):
         return umls_to_ddb
     
     def load_model(self, model, model_weights):
-        model = PeftModel.from_pretrained(model, model_weights).cuda()
+        # model = PeftModel.from_pretrained(model, model_weights).cuda()
+        model = PeftModel.from_pretrained(model, model_weights, device_map="auto", offload_folder="../offload")
         model.config.pad_token_id = self.tokenizer.eos_token_id 
         model = model.eval()
 
